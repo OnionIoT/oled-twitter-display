a70f17d6cffdca641a77d93b175c9071444f57fd
greenbreakfast <lazardemin@gmail.com>
2017-02-13 09:36:33 -0500

    implemented fetching last tweet and displaying on the OLED expansion

--- oledTwitterDisplay.py
+++ oledTwitterDisplay.py
@@ -3,16 +3,21 @@ import json
 import base64
 import urllib3
 http = urllib3.PoolManager()
+from OmegaExpansion import oledExp
 
+baseUrl     = "https://api.twitter.com"
+bearerToken = ""
+
+# function to perform applcation-only authentication with Twitter
 def twitterApiAuthenticate(consumerKey, consumerSecret):
-    url = "https://api.twitter.com/oauth2/token"
-    # RFC 1738 encode the consumer key and secret
-    #urllib.parse.urlencode({'site':'Stack Overflow'})
+    url = baseUrl + "/oauth2/token"
+    # in the future, may have to RFC 1738 encode the consumer key and secret
 
     # base64 encode the concetanated consumer key and secret
     consumerCredentials = consumerKey + ":" + consumerSecret
-
     encodedConsumerCredentials = base64.b64encode(consumerCredentials)
+
+    # create the request headers and body
     headers = {
         # 'authorization': {
         #     'Basic': encodedConsumerCredentials
@@ -22,22 +27,73 @@ def twitterApiAuthenticate(consumerKey, consumerSecret):
     }
     body = "grant_type=client_credentials"
 
-    # print "POST to %s:: headers = ", headers
-    # print "             body    = ", body
-
+    # perform the request
     r = http.request(
         'POST',
-        'https://api.twitter.com/oauth2/token',
+        url,
         headers=headers,
         body=body
     )
 
+    # convert the response data to a dictionary
     responseData = json.loads(r.data.decode('utf-8'))
 
     if 'token_type' in responseData and responseData['token_type'] == 'bearer' and 'access_token' in responseData:
-        return responseData['access_token']
+        global bearerToken
+        bearerToken = responseData['access_token']
+        return True
     else:
-        return responseData
+        bearerToken = responseData
+        return False
+
+# function to read the last tweet from a user's timeline
+def twitterApiGetLastTweet(userId):
+    url = baseUrl + '/1.1/statuses/user_timeline.json'
+    params = {
+        'screen_name': userId,
+        'count': 1
+    }
+
+    # create the request headers
+    headers = {
+        'Authorization': 'Bearer ' + bearerToken
+    }
+
+    # execute the GET request
+    r = http.request(
+        'GET',
+        url,
+        headers=headers,
+        fields=params
+    )
+
+    # convert the response data to a dictionary
+    responseData = json.loads(r.data.decode('utf-8'))
+    lastTweet = responseData[0]
+
+    if 'text' in lastTweet:
+        ret = {
+            'text': lastTweet['text'].encode('utf-8'),
+            'date': lastTweet['created_at']
+        }
+        return ret
+    else:
+        return False
+
+def oledWriteTweet(user, text, date):
+    if oledExp.driverInit() != 0:
+        print 'ERROR: Could not initialize the OLED Expansion'
+        return False
+
+    # write out the name of the account
+    oledExp.write('@' + user + ':')
+
+    # set the cursor to the next line
+    oledExp.setCursor(1,0)
+
+    # write out the tweet
+    oledExp.write(text)
+
 
 
 ### MAIN PROGRAM ###
@@ -50,11 +106,19 @@ with open( '/'.join([dirName, 'config.json']) ) as f:
 
 
 # authenticate with twitter
-authToken = twitterApiAuthenticate(config['authorization']['consumerKey'], config['authorization']['consumerSecret'])
-print "Auth Token: ", authToken
-
-if authToken == -1:
+authSuccess = twitterApiAuthenticate(config['authorization']['consumerKey'], config['authorization']['consumerSecret'])
+if not authSuccess:
     print "ERROR: Invalid API credentials!"
     exit()
 
-print "doing other stuff"
+# use twitter api to get last tweet of specified user
+tweet = twitterApiGetLastTweet(config['application']['user'])
+if not tweet:
+    print "ERROR: Could not retreive Tweet!"
+    exit()
+
+print 'Got tweet! ', tweet
+# display the tweet on the OLED
+oledWriteTweet(config['application']['user'], tweet['text'], tweet['date'])
+
+print 'Done!'
